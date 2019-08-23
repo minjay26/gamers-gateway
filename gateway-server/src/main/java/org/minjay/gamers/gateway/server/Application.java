972525eccfc33efb1d7c62d4aa73aa98d0149602
minjay26 <min.luo@tendata.cn>
2019-08-23 14:49:15 +0800

    集成spring cloud alibaba nacos服务发现

--- gateway-server/src/main/java/org/minjay/gamers/gateway/server/Application.java
+++ gateway-server/src/main/java/org/minjay/gamers/gateway/server/Application.java
@@ -1,8 +1,11 @@
 package org.minjay.gamers.gateway.server;
 
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
+import com.netflix.loadbalancer.IRule;
 import org.minjay.gamers.gateway.server.filter.ExtractTokenToJwtPreFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -10,6 +13,7 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
 
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class Application {
@@ -31,6 +35,11 @@ public class Application {
     @Bean
     public ExtractTokenToJwtPreFilter extractTokenToJwtFilter() {
         return new ExtractTokenToJwtPreFilter();
+    }
+
+    @Bean
+    public IRule rule() {
+        return new NacosRule();
     }
 
 //    @Bean
